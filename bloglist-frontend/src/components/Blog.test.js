e4225835e9a2706501116c4bba74f7d6518717cf
jkotima <jkotima@gmail.com>
2020-08-11 14:13:25 +0300

    5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,39 +1,55 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
-import { prettyDOM } from '@testing-library/dom'
+//import { prettyDOM } from '@testing-library/dom'
 
-test('renders blog title and author on default (without showing url and likes)', () => {
-  const blog = {
-    title: 'Test title',
-    author: 'Test author',
-    url: 'Test url',
-    likes: '12345',
-    user: {
+describe('<Blog />', () => {
+  let blog, user, mockHandler, component
+
+  beforeEach(() => {
+    blog = {
+      title: 'Test title',
+      author: 'Test author',
+      url: 'Test url',
+      likes: '12345',
+      user: {
+        username: 'Test user'
+      }
+
+    }
+    user = {
       username: 'Test user'
     }
+    mockHandler = jest.fn()
 
-  }
-  const user = {
-    username: 'Test user'
-  }
-  const mockHandler = jest.fn()
+    component = render(
+      <Blog blog={blog} likeBlog={mockHandler} removeBlog={mockHandler} user={user} />
+    )
+  })
 
+  test('renders blog title and author on default (without showing url and likes)', () => {
+    //console.log(prettyDOM(component.container))
 
-  const component = render(
-    <Blog blog={blog} likeBlog={mockHandler} removeBlog={mockHandler} user={user}/>
-  )
+    expect(component.container).toHaveTextContent('Test title')
+    expect(component.container).toHaveTextContent('Test author')
 
-  //console.log(prettyDOM(component.container))
+    const divWithUrl = component.getByText('Test url', { exact: false })
+    const divWithLikes = component.getByText('12345', { exact: false })
 
-  expect(component.container).toHaveTextContent('Test title')
-  expect(component.container).toHaveTextContent('Test author')
+    expect(divWithUrl).toHaveStyle('display: none')
+    expect(divWithLikes).toHaveStyle('display: none')
 
-  const divWithUrl = component.getByText('Test url', { exact: false })
-  const divWithLikes = component.getByText('12345', { exact: false })
+  })
 
-  expect(divWithUrl).toHaveStyle('display: none')
-  expect(divWithLikes).toHaveStyle('display: none')
+  test('after clicking view-button, url and likes are visible', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
 
+    const divWithUrl = component.getByText('Test url', { exact: false })
+    const divWithLikes = component.getByText('12345', { exact: false })
+
+    expect(divWithUrl).not.toHaveStyle('display: none')
+    expect(divWithLikes).not.toHaveStyle('display: none')
+  })
 })

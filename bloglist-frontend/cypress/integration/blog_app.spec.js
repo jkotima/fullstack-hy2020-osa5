eed81e83858b53e72ccd639485c80add70fa4c87
jkotima <jkotima@gmail.com>
2020-08-12 16:09:53 +0300

    5.18

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Test name',
+      username: 'Testusername',
+      password: 'Testpassword'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,25 @@ describe('Blog app', function() {
     cy.contains('login').click()
     cy.contains('username').should('be.visible')
     cy.contains('password').should('be.visible')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('Testusername')
+      cy.get('#password').type('Testpassword')
+      cy.get('#login-button').click()
+      cy.contains('Test name logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('jaaboldi')
+      cy.get('#password').type('kuuboldi')
+      cy.get('#login-button').click()
+      cy.contains('wrong username or password')
+      cy.get('.error').should('contain', 'wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
